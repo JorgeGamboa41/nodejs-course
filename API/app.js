c53957b14d528280e525e5c42425e354dd4b6a31
Jorge Gamboa <jorgegamboa41@gmail.com>
2021-01-10 17:06:49 -0400

    Final revisado

--- API/app.js
+++ API/app.js
@@ -1,23 +1,25 @@
 'use strict'
 
 const express = require('express')
-const bodyParser = require('body-parser') //Middleware 'body-parser' que nos permitirá tratar los datos que recibamos desde el cliente a través de las peticiones HTTP.
+const bodyParser = require('body-parser')
 const hbs = require('express-handlebars')
 const app = express()
 const api = require('./routes')
 
-
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.engine('.hbs', hbs({
-    defaultLayout: 'default',
-    extname: '.hbs'
+  defaultLayout: 'default',
+  extname: '.hbs'
 }))
 app.set('view engine', '.hbs')
 
 app.use('/api', api)
 app.get('/login', (req, res) => {
-    res.render('login')
+  res.render('login')
+})
+app.get('/', (req, res) => {
+  res.render('product')
 })
 
 module.exports = app
